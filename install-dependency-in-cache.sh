71390dd21776a990a90218f3d922e6f677e916dc
Fabien <contact@fabien-hebuterne.fr>
2022-08-18 23:11:12 +0200

    fix : fix ci using correct folder name

--- install-dependency-in-cache.sh
+++ install-dependency-in-cache.sh
@@ -13,8 +13,6 @@ mkdir ./libs && cd ./libs || exit
 
 # for latest version we use only private ci because it is recommended to use buildtools to download latest version and not CDN
 ## 1.18.2
-mkdir 1.18.2 && cd 1.18.2 || exit
-
 if [ -n "$1" ]; then
   echo "info : using private cache ci url for 1.18.2 - spigot 1.18.2"
   curl -O --silent "$1"common/1.18.2/spigot-1.18.2-R0.1-SNAPSHOT.jar -u "$2:$3"
@@ -22,9 +20,6 @@ if [ -n "$1" ]; then
 fi
 
 ## 1.19.2
-cd ..
-mkdir 1.19.2 && cd 1.19.2 || exit
-
 if [ -n "$1" ]; then
   echo "info : using private cache ci url for 1.19.2 - spigot 1.19.2"
   curl -O --silent "$1"common/1.19.2/spigot-1.19.2-R0.1-SNAPSHOT.jar -u "$2:$3"
