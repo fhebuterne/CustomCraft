a834d58d96557dc9a80555f3bd4cdf222288deb6
Fabien <contact@fabien-hebuterne.fr>
2023-09-09 23:41:06 +0200

    feat : add 1.20 compatibility - 1.5.0 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 allprojects {
     group = "fr.fabienhebuterne.customcraft"
-    version = "1.4.0"
+    version = "1.5.0"
 
     apply(plugin = "base")
     apply(plugin = "java")
--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
     implementation(project(":nms:v1_19_R1"))
     implementation(project(":nms:v1_19_R2"))
     implementation(project(":nms:v1_19_R3"))
+    implementation(project(":nms:v1_20_R1"))
 }
 
 tasks.withType<Jar> {
